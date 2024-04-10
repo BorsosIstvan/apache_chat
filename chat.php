156a3591c297300beb85969a18f69b56c5de60ab
BorsosIstvan <bistvanus@gmail.com>
2024-04-10 17:17:20 +0200

    php en ajax

--- chat.php
+++ chat.php
@@ -1,13 +1,27 @@
 <?php
 // Check of het verzoek een POST-verzoek is met een bericht
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["message"])) {
-    // Ontvang het bericht van de client
+    // Ontvang de gebruikersnaam en het bericht van de client
+    $username = $_POST["username"];
     $message = $_POST["message"];
     // Voeg het bericht toe aan het chatbestand
-    file_put_contents("chat.txt", $message . PHP_EOL, FILE_APPEND);
+    file_put_contents("chat.txt", json_encode(["username" => $username, "message" => $message]) . PHP_EOL, FILE_APPEND);
 }
 
 // Laad de berichten uit het chatbestand en stuur ze naar de client
 $chatContents = file_get_contents("chat.txt");
-echo $chatContents;
+// Parse de berichten als JSON
+$messages = [];
+$lines = explode(PHP_EOL, $chatContents);
+foreach ($lines as $line) {
+    if (!empty($line)) {
+        $messageData = json_decode($line, true);
+        if ($messageData) {
+            $messages[] = $messageData;
+        }
+    }
+}
+// Stuur de berichten terug naar de client als JSON
+header("Content-Type: application/json");
+echo json_encode($messages);
 ?>
