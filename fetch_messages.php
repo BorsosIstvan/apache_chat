06a5959b742307f3c7052d8ddfcd6d835e7524d0
BorsosIstvan <bistvanus@gmail.com>
2024-04-10 19:23:54 +0200

    hoi

--- fetch_messages.php
+++ fetch_messages.php
@@ -1,6 +1,8 @@
 <?php
 // Lees berichten uit het JSON-bestand
 $messages = [];
+if (!file_exists('messages.json')) {
+    file_put_contents('messages.json', '[]');
 if (file_exists('messages.json')) {
     $messages = json_decode(file_get_contents('messages.json'), true);
 }
