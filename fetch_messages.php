109f0f9f9f0207cca9cd4f60476aacaeb583f103
BorsosIstvan <bistvanus@gmail.com>
2024-04-11 09:22:40 +0200

    Het werkt!

--- fetch_messages.php
+++ fetch_messages.php
@@ -1,8 +1,8 @@
 <?php
 // Lees berichten uit het JSON-bestand
 $messages = [];
-if (file_exists('/home/pi/poci/apache_chat/messages.json')) {
-    $messages = json_decode(file_get_contents('/home/pi/poci/apache_chat/messages.json'), true);
+if (file_exists('messages.json')) {
+    $messages = json_decode(file_get_contents('messages.json'), true);
 }
 
 // Retourneer berichten als JSON
